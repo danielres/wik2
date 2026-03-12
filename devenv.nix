e86ab8f1a5d185dddaaf9a67ec05af85f4d48dff
Daniel Reszka <dannn.r@gmail.com>
2026-03-12 14:47:09 +0100

    devenv: disable all non-elixir

--- devenv.nix
+++ devenv.nix
@@ -5,10 +5,10 @@
     [
       #
       git
-      lefthook
-      nodejs_24
-      pgcli
-      secretspec
+      # lefthook
+      # nodejs_24
+      # pgcli
+      # secretspec
     ] ++ lib.optionals pkgs.stdenv.isLinux [ inotify-tools libnotify ]
     ++ lib.optionals pkgs.stdenv.isDarwin [
       terminal-notifier
@@ -18,22 +18,22 @@
 
   languages.elixir = { enable = true; };
 
-  services.adminer.enable = true;
+  # services.adminer.enable = true;
 
-  services.postgres = {
-    enable = true;
-    port = 5432;
-    initialDatabases = [ { name = "wik_dev"; } { name = "wik_test"; } ];
-    initialScript = ''
-      CREATE ROLE postgres SUPERUSER LOGIN;  
-    '';
-  };
+  # services.postgres = {
+  #   enable = true;
+  #   port = 5432;
+  #   initialDatabases = [ { name = "wik_dev"; } { name = "wik_test"; } ];
+  #   initialScript = ''
+  #     CREATE ROLE postgres SUPERUSER LOGIN;  
+  #   '';
+  # };
 
-  processes.phx-server = {
-    exec = "PORT=4000 mix phx.server";
-    # if your Phoenix app is in a subdir, set cwd explicitly, e.g.:
-    # cwd = "${config.git.root}/youmap";
-  };
+  # processes.phx-server = {
+  #   exec = "PORT=4000 mix phx.server";
+  #   # if your Phoenix app is in a subdir, set cwd explicitly, e.g.:
+  #   # cwd = "${config.git.root}/youmap";
+  # };
 
   process.manager.implementation = "overmind";
 

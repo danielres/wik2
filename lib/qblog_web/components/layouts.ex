e0d8946b4332e8cf3db55035f6dc04a3bc6820f0
Daniel Reszka <dannn.r@gmail.com>
2026-03-13 15:18:19 +0100

    layout: remove boilerplate

--- lib/qblog_web/components/layouts.ex
+++ lib/qblog_web/components/layouts.ex
@@ -39,9 +39,8 @@ defmodule QblogWeb.Layouts do
       <div class="flex-1">
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
           <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
           <div class="opacity-80">
-            <span class="font-bold tracking-wider opacity-50">/{@scope.tenant}</span>
+            <span class="font-bold tracking-wide opacity-50">/{@scope.tenant}</span>
           </div>
         </a>
       </div>
@@ -49,18 +48,7 @@ defmodule QblogWeb.Layouts do
       <div class="flex-none">
         <ul class="flex flex-column px-1 space-x-4 items-center">
           <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
             <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
           </li>
         </ul>
       </div>

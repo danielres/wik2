bad24901ee7d33db873d29b271f6234eeef8cd4a
Daniel Reszka <dannn.r@gmail.com>
2026-03-20 16:15:29 +0100

    page_tree_live/helpers.ex: parent_options: add parent_id to forbidden

--- lib/qblog_web/live/page_tree_live/helpers.ex
+++ lib/qblog_web/live/page_tree_live/helpers.ex
@@ -2,7 +2,10 @@ defmodule QblogWeb.PageTreeLive.Helpers do
   def has_children?(node), do: node.children != []
 
   def parent_options(nodes, node_id) do
-    forbidden_ids = [node_id | descendants(nodes, node_id)]
+    node = nodes |> Enum.find(&(&1.id == node_id))
+    parent_id = node.parent_id
+
+    forbidden_ids = [node_id, parent_id] ++ descendants(nodes, node_id)
 
     Enum.reject(nodes, &(&1.id in forbidden_ids))
   end

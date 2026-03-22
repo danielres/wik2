a11f3bc83101aecd24ec5f4bb7a5004cdb6717f9
Daniel Reszka <dannn.r@gmail.com>
2026-03-22 11:08:11 +0100

    components/modal.ex: add

---
+++ lib/qblog_web/components/modal.ex
@@ -0,0 +1,36 @@
+defmodule QblogWeb.Components.Modal do
+  use Phoenix.Component
+
+  attr :"phx-target", :any, required: false
+  attr :cancel, :string, required: false
+  attr :open?, :boolean, default: true
+  slot :inner_block, required: true
+
+  def render(assigns) do
+    ~H"""
+    <dialog
+      class={["modal", @open? and "modal-open"]}
+      phx-key="escape"
+      phx-target={assigns[:"phx-target"]}
+      phx-window-keydown={@cancel}
+    >
+      <div
+        :if={@open?}
+        class={["modal-box", "min-w-sm", "bg-base-200"]}
+        phx-click-away={@cancel}
+        phx-target={assigns[:"phx-target"]}
+      >
+        <button
+          phx-click={@cancel}
+          phx-target={assigns[:"phx-target"]}
+          class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+        >
+          ✕
+        </button>
+
+        {render_slot(@inner_block)}
+      </div>
+    </dialog>
+    """
+  end
+end

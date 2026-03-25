77b2cb81d4e4b558d190e3a5015d8a6c93bf8f15
Daniel Reszka <dannn.r@gmail.com>
2026-03-25 15:38:34 +0100

    page_tree_live/helpers.ex: also reject parents having a child with same slug

--- lib/qblog_web/live/page_tree_live/helpers.ex
+++ lib/qblog_web/live/page_tree_live/helpers.ex
@@ -11,19 +11,30 @@ defmodule QblogWeb.PageTreeLive.Helpers do
 
   def parent_options(nodes, node_id) do
     node = nodes |> Enum.find(&(&1.id == node_id))
+    slug = node.slug
     parent_id = node.parent_id
 
-    forbidden_ids = [node_id, parent_id] ++ descendants(nodes, node_id)
+    forbidden_ids =
+      [node_id, parent_id] ++
+        descendant_ids(node_id, nodes) ++
+        parent_ids_with_child_slug(slug, nodes)
 
     Enum.reject(nodes, &(&1.id in forbidden_ids))
   end
 
-  defp descendants(nodes, root_id) do
+  defp descendant_ids(root_id, nodes) do
     children =
       nodes
       |> Enum.filter(&(&1.parent_id == root_id))
       |> Enum.map(& &1.id)
 
-    children ++ Enum.flat_map(children, &descendants(nodes, &1))
+    children ++ Enum.flat_map(children, &descendant_ids(&1, nodes))
+  end
+
+  defp parent_ids_with_child_slug(slug, nodes) do
+    nodes
+    |> Enum.filter(&(&1.slug == slug))
+    |> Enum.map(& &1.parent_id)
+    |> Enum.reject(&is_nil/1)
   end
 end

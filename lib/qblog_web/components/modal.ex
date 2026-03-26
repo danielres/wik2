7c74d32ad725ca8e702e66c273e722072cdebcc3
Daniel Reszka <dannn.r@gmail.com>
2026-03-26 14:24:18 +0100

    modal: make :title optional

--- lib/qblog_web/components/modal.ex
+++ lib/qblog_web/components/modal.ex
@@ -5,7 +5,7 @@ defmodule QblogWeb.Components.Modal do
   attr :cancel, :string, required: false
   attr :open?, :boolean, default: true
   slot :inner_block, required: true
-  slot :title, required: true
+  slot :title, required: false
 
   def render(assigns) do
     ~H"""
@@ -34,7 +34,7 @@ defmodule QblogWeb.Components.Modal do
           ✕
         </button>
 
-        <h3 class="mb-2">{render_slot(@title)}</h3>
+        <h3 :if={@title != []} class="mb-2">{render_slot(@title)}</h3>
 
         {render_slot(@inner_block)}
       </div>

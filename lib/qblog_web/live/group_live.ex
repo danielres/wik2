1e19ca10aaef0d8cb43ae79416313f5c8003d2bf
Daniel Reszka <dannn.r@gmail.com>
2026-03-31 17:06:07 +0200

    group_live.ex: render memberships with details (type + joined date)

--- lib/qblog_web/live/group_live.ex
+++ lib/qblog_web/live/group_live.ex
@@ -6,7 +6,7 @@ defmodule QblogWeb.GroupLive do
   def mount(_params, _session, socket) do
     scope = socket.assigns.current_scope
     group = socket.assigns.current_scope.tenant
-    group = Ash.load!(group, [:users], scope: scope)
+    group = Ash.load!(group, [memberships: [:user]], scope: scope)
     {:ok, socket |> assign(group: group)}
   end
 
@@ -26,9 +26,28 @@ defmodule QblogWeb.GroupLive do
           <div class="card-body">
             <h2 class="text-xl">Members</h2>
 
-            <ul class="space-y-1 flex gap-1 flex-wrap">
-              <li :for={member <- @group.users} class="badge bg-base-200 px-3 rounded">
-                {member |> to_string()}
+            <ul class="space-y-0.5">
+              <li
+                :for={membership <- @group.memberships}
+                class={[
+                  "flex items-center justify-between gap-1 flex-wrap",
+                  "rounded bg-base-200/50 px-3 py-2"
+                ]}
+              >
+                <span>{membership.user |> to_string()}</span>
+
+                <span class={[
+                  "flex flex-wrap gap-1",
+                  "text-sm opacity-70"
+                ]}>
+                  <span class={["badge badge-sm px-2 bg-base-300"]}>
+                    {membership.type |> Atom.to_string() |> String.capitalize()}
+                  </span>
+
+                  <span class={["badge badge-sm px-2 bg-base-300", "whitespace-nowrap"]}>
+                    Since {Calendar.strftime(membership.inserted_at, "%Y-%m-%d %H:%M")}
+                  </span>
+                </span>
               </li>
             </ul>
           </div>

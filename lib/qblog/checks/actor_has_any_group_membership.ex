e42716c28f7ff9e93b9edeb3796af6fbd5bf4c5c
Daniel Reszka <dannn.r@gmail.com>
2026-03-31 13:12:28 +0200

    checks/actor_has_any_group_membership.ex: add

---
+++ lib/qblog/checks/actor_has_any_group_membership.ex
@@ -0,0 +1,21 @@
+defmodule Qblog.Checks.ActorHasAnyGroupMembership do
+  use Ash.Policy.SimpleCheck
+
+  require Ash.Query
+
+  alias Qblog.Accounts.GroupUserRelation
+
+  @impl true
+  def describe(_opts), do: "actor belongs to at least one group"
+
+  @impl true
+  def match?(nil, _context, _opts), do: {:ok, false}
+
+  def match?(actor, _context, _opts) do
+    query =
+      GroupUserRelation
+      |> Ash.Query.filter(user_id == ^actor.id)
+
+    Ash.exists(query, authorize?: false)
+  end
+end

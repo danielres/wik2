1e983e2e120af58fc50840a28743233ed5da63d5
Daniel Reszka <dannn.r@gmail.com>
2026-03-31 13:32:03 +0200

    group_live.ex: render members list

--- lib/qblog_web/live/group_live.ex
+++ lib/qblog_web/live/group_live.ex
@@ -4,9 +4,10 @@ defmodule QblogWeb.GroupLive do
   on_mount {QblogWeb.LiveUserAuth, :live_scope_required}
 
   def mount(_params, _session, socket) do
-    # scope = socket.assigns.current_scope
-
-    {:ok, socket}
+    scope = socket.assigns.current_scope
+    group = socket.assigns.current_scope.tenant
+    group = Ash.load!(group, [:users], scope: scope)
+    {:ok, socket |> assign(group: group)}
   end
 
   def render(assigns) do
@@ -20,6 +21,18 @@ defmodule QblogWeb.GroupLive do
             <span>group details</span>
           </span>
         </h1>
+
+        <div class="card card-sm bg-base-100">
+          <div class="card-body">
+            <h2 class="text-xl">Members</h2>
+
+            <ul class="space-y-1 flex gap-1 flex-wrap">
+              <li :for={member <- @group.users} class="badge bg-base-200 px-3 rounded">
+                {member |> to_string()}
+              </li>
+            </ul>
+          </div>
+        </div>
       </Layouts.group>
     </Layouts.app>
     """

22c6a35ad0c8c2f1ec10135db68785fb953faf90
Daniel Reszka <dannn.r@gmail.com>
2026-04-04 22:02:37 +0200

    fix failing tests

--- test/support/live/page_tree_editor_test_live.ex
+++ test/support/live/page_tree_editor_test_live.ex
@@ -1,11 +1,13 @@
 defmodule QblogWeb.PageTreeEditorTestLive do
   use QblogWeb, :live_view
 
+  alias Qblog.Accounts
   alias Qblog.Wiki
   alias QblogWeb.PageTreeLive.PageTreeEditor
 
   @impl true
-  def mount(_params, %{"tenant" => tenant}, socket) do
+  def mount(_params, %{"tenant" => tenant_name}, socket) do
+    {:ok, tenant} = Accounts.get_group_by_name(tenant_name, authorize?: false)
     current_scope = %{tenant: tenant}
     {:ok, page_tree} = Wiki.get_page_tree(scope: current_scope)
 

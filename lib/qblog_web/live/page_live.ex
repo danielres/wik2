2cb6547809bde00a7764e94afe017b07af6ebc7c
Daniel Reszka <dannn.r@gmail.com>
2026-04-12 15:43:57 +0200

    page_live.ex: refactor to pure orchestrator

--- lib/qblog_web/live/page_live.ex
+++ lib/qblog_web/live/page_live.ex
@@ -4,7 +4,6 @@ defmodule QblogWeb.PageLive do
   use QblogWeb, :live_view
 
   alias Qblog.Wiki
-  alias QblogWeb.Components
   alias QblogWeb.Endpoint
   alias QblogWeb.PageLive.BlockActions
   alias QblogWeb.PageLive.BlockEdit
@@ -21,18 +20,15 @@ defmodule QblogWeb.PageLive do
     scope = socket.assigns.current_scope
     {node, page} = scope |> PageState.load_page_and_node_by_path(path)
 
-    page_exists? = page != nil
-
-    if page_exists? or Ash.can?({Wiki.Page, :create}, scope) do
+    if page != nil or Ash.can?({Wiki.Page, :create}, scope) do
       socket =
         socket
-        |> assign(path: path, node: node, page: page)
+        |> assign(node: node, page: page)
         |> assign(editing_block_id: nil, form_edit_block: nil, editing?: false)
         |> PageState.sync_block_subscriptions(page)
         |> Locks.assign_locks()
 
       if connected?(socket), do: Endpoint.subscribe("block_placement:page:#{page.id}")
-
       {:ok, socket}
     else
       socket = socket |> assign(not_found_path: path)
@@ -40,116 +36,9 @@ defmodule QblogWeb.PageLive do
     end
   end
 
-  def render(%{not_found_path: _not_found_path} = assigns) do
-    ~H"""
-    <Layouts.app flash={@flash} scope={@current_scope}>
-      <Layouts.group presences={@presences} scope={@current_scope} view="wiki">
-        <div class="card-body bg-base-200 rounded flex flex-col items-center gap-4 py-16">
-          <div>Ooops...</div>
-          <div class="font-bold">"{@not_found_path}"</div>
-          <div>Could not be found</div>
-        </div>
-      </Layouts.group>
-    </Layouts.app>
-    """
-  end
-
-  @impl true
-  def render(assigns) do
-    ~H"""
-    <Layouts.app flash={@flash} scope={@current_scope}>
-      <Layouts.group presences={@presences} scope={@current_scope} view="wiki">
-        <section class="space-y-8">
-          <header class="space-y-1">
-            <div class="flex justify-between gap-4">
-              <h1 class="text-2xl">{@node.title}</h1>
-              <button
-                class={[
-                  "btn btn-sm btn-square btn-xs",
-                  "opacity-80 hover:opacity-100",
-                  @editing? && "btn-primary",
-                  !@editing? && "btn-ghost"
-                ]}
-                phx-click="toggle_edit_mode"
-              >
-                <.icon name="hero-pencil-square-mini" />
-              </button>
-            </div>
-            <div class="text-sm opacity-60">page author: {@page.author |> to_string()}</div>
-            <div class="text-sm opacity-60">
-              inserted_at: {@page.inserted_at |> Utils.Time.relative()}
-            </div>
-          </header>
-
-          <div class={[
-            "grid gap-8 sm:grid-cols-2"
-          ]}>
-            <div
-              :for={placement <- @page.block_placements}
-              class={[
-                "card rounded col-span-1",
-                placement.width == "half" && "sm:col-span-1",
-                placement.width == "full" && "sm:col-span-2"
-              ]}
-              id={"block-#{placement.block.id}"}
-            >
-              <div class="card-body py-0.5 px-0 ">
-                <%= if @editing_block_id == placement.block.id do %>
-                  <Components.Block.form placement={placement} form={@form_edit_block} />
-                <% else %>
-                  <Components.Block.render
-                    editing?={@editing?}
-                    lock={@locks[placement.block.id]}
-                    placement={placement}
-                  />
-                <% end %>
-              </div>
-            </div>
-
-            <div
-              :if={Enum.empty?(@page.block_placements)}
-              class={[
-                "sm:col-span-2",
-                "p-8",
-                "rounded bg-base-300/30",
-                "text-center"
-              ]}
-            >
-              No blocks yet
-            </div>
-          </div>
-
-          <div
-            :if={@editing?}
-            class="flex justify-end join"
-          >
-            <.button
-              class="btn btn-sm btn-primary join-item"
-              phx-click="add_block"
-              phx-value-type="text"
-            >
-              Add block
-            </.button>
-
-            <Components.Block.AddBlockMenuButton.render
-              id="popover-special-blocks"
-              class={[
-                "btn btn-sm btn-primary join-item",
-                "btn-square",
-                "border-l border-base-300"
-              ]}
-              open?={false}
-            />
-          </div>
-        </section>
-      </Layouts.group>
-    </Layouts.app>
-    """
-  end
-
   @impl true
   def handle_params(_params, url, socket) do
-    {:noreply, Presence.track_in_liveview(socket, url)}
+    {:noreply, socket |> Presence.track_in_liveview(url)}
   end
 
   @impl true
@@ -219,17 +108,17 @@ defmodule QblogWeb.PageLive do
   end
 
   @impl true
-  def handle_info({QblogWeb.Presence, {:join, _presence}}, socket) do
+  def handle_info({Presence, {:join, _presence}}, socket) do
     {:noreply, socket |> Locks.refresh_presence()}
   end
 
   @impl true
-  def handle_info({QblogWeb.Presence, {:leave, _presence}}, socket) do
+  def handle_info({Presence, {:leave, _presence}}, socket) do
     {:noreply, socket |> Locks.refresh_presence()}
   end
 
   @impl true
-  def handle_info({QblogWeb.Presence, {:update, %{id: _id, meta: _meta}}}, socket) do
+  def handle_info({Presence, {:update, %{id: _id, meta: _meta}}}, socket) do
     {:noreply, socket |> Locks.refresh_presence()}
   end
 end

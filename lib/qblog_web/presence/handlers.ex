f27f7fec567a24a509c1d623829c1911d8ea6331
Daniel Reszka <dannn.r@gmail.com>
2026-04-12 15:49:19 +0200

    presence/handlers.ex: cleanup

--- lib/qblog_web/presence/handlers.ex
+++ lib/qblog_web/presence/handlers.ex
@@ -5,10 +5,12 @@ defmodule QblogWeb.Presence.Handlers do
 
   import Phoenix.Component, only: [assign: 3]
 
+  alias QblogWeb.Presence
+
   def handle_presence_change(socket) do
     case socket.assigns[:current_scope] do
       %{tenant: %{id: group_id}} ->
-        assign(socket, :presences, QblogWeb.Presence.list_online_users_in_group(group_id))
+        assign(socket, :presences, Presence.list_online_users_in_group(group_id))
 
       _ ->
         assign(socket, :presences, [])
@@ -17,16 +19,16 @@ defmodule QblogWeb.Presence.Handlers do
 
   defmacro __using__(_opts) do
     quote do
-      def handle_info({QblogWeb.Presence, {:join, _presence}}, socket) do
-        {:noreply, QblogWeb.Presence.Handlers.handle_presence_change(socket)}
+      def handle_info({Presence, {:join, _presence}}, socket) do
+        {:noreply, Presence.Handlers.handle_presence_change(socket)}
       end
 
-      def handle_info({QblogWeb.Presence, {:leave, _presence}}, socket) do
-        {:noreply, QblogWeb.Presence.Handlers.handle_presence_change(socket)}
+      def handle_info({Presence, {:leave, _presence}}, socket) do
+        {:noreply, Presence.Handlers.handle_presence_change(socket)}
       end
 
-      def handle_info({QblogWeb.Presence, {:update, %{id: _id, meta: _meta}}}, socket) do
-        {:noreply, QblogWeb.Presence.Handlers.handle_presence_change(socket)}
+      def handle_info({Presence, {:update, %{id: _id, meta: _meta}}}, socket) do
+        {:noreply, Presence.Handlers.handle_presence_change(socket)}
       end
     end
   end

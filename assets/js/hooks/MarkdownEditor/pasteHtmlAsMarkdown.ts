6f9adbc06cfe5a8607fceedf25e37f3d5f9f2390
Daniel Reszka <dannn.r@gmail.com>
2026-04-20 01:04:34 +0200

    pasteHtmlAsMarkdown.ts: make markdown style from html paste consistent

--- assets/js/hooks/MarkdownEditor/pasteHtmlAsMarkdown.ts
+++ assets/js/hooks/MarkdownEditor/pasteHtmlAsMarkdown.ts
@@ -1,7 +1,22 @@
 import { EditorView } from "@codemirror/view";
 import TurndownService from "turndown";
 
-const turndown = new TurndownService();
+const turndown = new TurndownService({
+  bulletListMarker: "-",
+  codeBlockStyle: "fenced",
+  emDelimiter: "_",
+  headingStyle: "atx",
+  strongDelimiter: "**",
+});
+
+turndown.addRule("removeEmptyLinks", {
+  filter(node) {
+    return node.nodeName == "A" && (node.textContent?.trim() ?? "") == "";
+  },
+  replacement() {
+    return "";
+  },
+});
 
 function htmlToMarkdown(html: string): string {
   return turndown.turndown(html).trim();
--- assets/js/types/turndown.d.ts
+++ assets/js/types/turndown.d.ts
@@ -1,6 +1,12 @@
 declare module "turndown" {
+  type TurndownRule = {
+    filter: (node: Node) => boolean;
+    replacement: (content: string, node: Node) => string;
+  };
+
   export default class TurndownService {
     constructor(options?: Record<string, unknown>);
+    addRule(key: string, rule: TurndownRule): this;
     turndown(input: string | Node): string;
   }
 }

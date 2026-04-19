bffb59f82943b135b6dbfe9939e589193e7fed4a
Daniel Reszka <dannn.r@gmail.com>
2026-04-19 19:30:38 +0200

    Cinder.Themes.Dense: loader: hide

--- lib/qblog_web/cinder/themes/dense.ex
+++ lib/qblog_web/cinder/themes/dense.ex
@@ -6,4 +6,5 @@ defmodule QblogWeb.Cinder.Themes.Dense do
   set :table_class, "table table-xs"
   set :th_class, "py-1 px-2"
   set :td_class, "py-1 px-2"
+  set :loading_container_class, "hidden"
 end

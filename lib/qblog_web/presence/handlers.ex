1a1ff6cfa4f9be0faf13365fda1b23c9c8b22221
Daniel Reszka <dannn.r@gmail.com>
2026-04-20 14:15:05 +0200

    delay presence removal
    * fixes jittery presence avatars component

--- lib/qblog_web/presence/handlers.ex
+++ lib/qblog_web/presence/handlers.ex
@@ -7,10 +7,17 @@ defmodule QblogWeb.Presence.Handlers do
 
   alias QblogWeb.Presence
 
+  @stale_presence_ttl_ms 250
+
   def handle_presence_change(socket) do
     case socket.assigns[:current_scope] do
       %{tenant: %{id: group_id}} ->
-        assign(socket, :presences, Presence.list_online_users_in_group(group_id))
+        presences = Presence.list_online_users_in_group(group_id)
+        stale_presences = socket.assigns[:stale_presences] || %{}
+
+        socket
+        |> assign(:presences, merge_presences(presences, stale_presences))
+        |> assign(:stale_presences, stale_presences)
 
       _ ->
         assign(socket, :presences, [])
@@ -26,19 +33,68 @@ defmodule QblogWeb.Presence.Handlers do
       defoverridable handle_presence_change: 1
 
       @impl true
-      def handle_info({QblogWeb.Presence, {:join, _presence}}, socket) do
-        {:noreply, handle_presence_change(socket)}
+      def handle_info({QblogWeb.Presence, {:join, presence}}, socket) do
+        socket =
+          socket
+          |> QblogWeb.Presence.Handlers.forget_stale_presence(presence.id)
+          |> handle_presence_change()
+
+        {:noreply, socket}
       end
 
       @impl true
-      def handle_info({QblogWeb.Presence, {:leave, _presence}}, socket) do
-        {:noreply, handle_presence_change(socket)}
+      def handle_info({QblogWeb.Presence, {:leave, presence}}, socket) do
+        socket =
+          socket
+          |> QblogWeb.Presence.Handlers.delay_presence_removal(presence)
+          |> handle_presence_change()
+
+        {:noreply, socket}
       end
 
       @impl true
-      def handle_info({QblogWeb.Presence, {:update, %{id: _id, meta: _meta}}}, socket) do
-        {:noreply, handle_presence_change(socket)}
+      def handle_info({QblogWeb.Presence, {:update, %{id: id}}}, socket) do
+        socket =
+          socket
+          |> QblogWeb.Presence.Handlers.forget_stale_presence(id)
+          |> handle_presence_change()
+
+        {:noreply, socket}
+      end
+
+      @impl true
+      def handle_info({QblogWeb.Presence, {:remove_stale, presence_id}}, socket) do
+        socket =
+          socket
+          |> QblogWeb.Presence.Handlers.forget_stale_presence(presence_id)
+          |> handle_presence_change()
+
+        {:noreply, socket}
       end
     end
   end
+
+  def delay_presence_removal(socket, presence) do
+    Process.send_after(self(), {Presence, {:remove_stale, presence.id}}, @stale_presence_ttl_ms)
+
+    socket
+    |> assign(:stale_presences, Map.put(stale_presences(socket), presence.id, presence))
+  end
+
+  def forget_stale_presence(socket, presence_id) do
+    socket
+    |> assign(:stale_presences, Map.delete(stale_presences(socket), presence_id))
+  end
+
+  defp merge_presences(presences, stale_presences) do
+    live_presence_ids = presences |> MapSet.new(& &1.id)
+
+    stale_presences
+    |> Map.values()
+    |> Enum.reject(&MapSet.member?(live_presence_ids, &1.id))
+    |> then(&(presences ++ &1))
+    |> Enum.sort_by(& &1.id)
+  end
+
+  defp stale_presences(socket), do: socket.assigns[:stale_presences] || %{}
 end

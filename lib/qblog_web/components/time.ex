b88fdd2d0010930092147d9f0702dbb0fe6d3bb3
Daniel Reszka <dannn.r@gmail.com>
2026-04-25 13:00:23 +0200

    Components.Time.relative_and_precise: add

---
+++ lib/qblog_web/components/time.ex
@@ -0,0 +1,43 @@
+defmodule QblogWeb.Components.Time do
+  @moduledoc """
+  Renders a date and time in a human-friendly format.
+  """
+  use QblogWeb, :html
+
+  attr :datetime, :string, required: true
+  attr :ago?, :boolean, default: false
+  attr :direction, :string, default: "bottom"
+
+  def relative_and_precise(assigns) do
+    direction_class =
+      case assigns[:direction] do
+        "left" -> "tooltip-left"
+        "right" -> "tooltip-right"
+        "top" -> "tooltip-top"
+        "bottom" -> "tooltip-bottom"
+        _ -> "tooltip-bottom"
+      end
+
+    assigns = assigns |> assign(direction_class: direction_class)
+
+    ~H"""
+    <span>
+      <span class={[
+        "badge badge-sm px-2 bg-base-300",
+        "opacity-60 hover:opacity-100 transition-opacity",
+        "whitespace-nowrap",
+        "tooltip tooltip-delayed tooltip-xs",
+        @direction_class,
+        "cursor-default"
+      ]}>
+        {Utils.Time.relative(@datetime)}
+
+        <div class="tooltip-content text-xs">
+          {Utils.Time.precise(@datetime)}
+        </div>
+      </span>
+      <span :if={@ago?}>ago</span>
+    </span>
+    """
+  end
+end

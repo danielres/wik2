69fca0441e6f9659738158892126a25191165a09
Daniel Reszka <dannn.r@gmail.com>
2026-05-02 22:59:26 +0200

    dev: add UX to sign in as any user

--- lib/qblog/dev_auth.ex
+++ lib/qblog/dev_auth.ex
@@ -6,6 +6,23 @@ defmodule Qblog.DevAuth do
 
   @superadmin_email "dev-superadmin@local.dev"
 
+  def list_sign_in_users do
+    User
+    |> Ash.read(authorize?: false, domain: Accounts)
+    |> case do
+      {:ok, users} -> {:ok, Enum.sort_by(users, &user_sort_key/1)}
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  def sign_in_user(user_id) when is_binary(user_id) do
+    case load_user_by_id(user_id) do
+      {:ok, nil} -> {:error, :user_not_found}
+      {:ok, user} -> {:ok, user}
+      {:error, error} -> {:error, error}
+    end
+  end
+
   def sign_in_superadmin do
     case load_user_by_email(@superadmin_email) do
       {:ok, nil} -> create_superadmin()
@@ -21,6 +38,12 @@ defmodule Qblog.DevAuth do
     |> Ash.read_one(authorize?: false, domain: Accounts)
   end
 
+  defp load_user_by_id(user_id) do
+    User
+    |> Ash.Query.filter(id == ^user_id)
+    |> Ash.read_one(authorize?: false, domain: Accounts)
+  end
+
   defp create_superadmin do
     Ash.create(
       User,
@@ -30,4 +53,14 @@ defmodule Qblog.DevAuth do
       domain: Accounts
     )
   end
+
+  defp user_sort_key(user) do
+    {role_sort_value(user.role), normalized_email(user.email), user.id}
+  end
+
+  defp role_sort_value(:superadmin), do: 0
+  defp role_sort_value(_role), do: 1
+
+  defp normalized_email(nil), do: "~"
+  defp normalized_email(email), do: email |> to_string() |> String.downcase()
 end

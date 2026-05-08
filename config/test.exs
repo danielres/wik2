4b73aaf233904d5ed3676a69483812560f450913
Daniel Reszka <dannn.r@gmail.com>
2026-05-08 22:53:08 +0200

    test.exs: disable error_tracker in tests

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,7 @@
 import Config
+
+config :error_tracker, enabled: false
+
 config :wik, token_signing_secret: "0RaegrszeoUZf8/Nmq0S/siUPZpCVgvn"
 config :bcrypt_elixir, log_rounds: 1
 config :ash, policies: [show_policy_breakdowns?: true], disable_async?: true

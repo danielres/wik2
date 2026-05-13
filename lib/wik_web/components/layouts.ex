b0cc0a9e9d78c71d96b487b8ae8914accf630352
Daniel Reszka <dannn.r@gmail.com>
2026-05-13 14:36:32 +0200

    Layouts.group: fix Wiki menu item to getting active styles

--- lib/wik_web/components/layouts.ex
+++ lib/wik_web/components/layouts.ex
@@ -98,7 +98,7 @@ defmodule WikWeb.Layouts do
     ]}>
       <menu class={[]}>
         <ul class="menu menu-horizontal gap-1">
-          <.menu_item tenant={@scope.tenant} view={@view} target="wiki/home">Wiki</.menu_item>
+          <.menu_item tenant={@scope.tenant} view={@view} target="wiki">Wiki</.menu_item>
           <.menu_item tenant={@scope.tenant} view={@view} target="events">Events</.menu_item>
           <.menu_item
             :if={@scope.actor.role == :superadmin}

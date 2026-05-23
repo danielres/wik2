63bc46960ab85c43fc4642c71ec6db9c831274ed
Daniel Reszka <dannn.r@gmail.com>
2026-05-23 13:07:08 +0200

    new_owner_selector.ex: render admins only for membership transfer

--- lib/wik_web/live/space_live/new_owner_selector.ex
+++ lib/wik_web/live/space_live/new_owner_selector.ex
@@ -5,15 +5,15 @@ defmodule WikWeb.SpaceLive.NewOwnerSelector do
   attr :memberships, :list, required: true
 
   def render(assigns) do
-    non_owners = assigns.memberships |> Enum.filter(&(&1.type != :owner))
+    non_owners = assigns.memberships |> Enum.filter(&(&1.type == :admin))
     assigns = assigns |> assign(non_owners: non_owners)
 
     ~H"""
     <div class="alert bg-error/50 text-error-content mb-4">
-      <.icon name="hero-exclamation-circle-micro self-start" class="size-6 opacity-50" />
+      <.icon name="hero-exclamation-triangle-micro self-start" class="size-6 opacity-50" />
 
       <div class="leading-tight space-y-2">
-        <p class="font-bold">This action will transfer ownership to the selected member.</p>
+        <p class="font-bold">This action will transfer ownership to the selected admin.</p>
         <p>
           You will become administrator of the space, and won't be able to transfer ownership again unless the new owner transfers it back to you.
         </p>
@@ -22,9 +22,9 @@ defmodule WikWeb.SpaceLive.NewOwnerSelector do
 
     <h3 class="text-xl mb-2">Select new owner</h3>
 
-    <ul class="space-y-0.5">
+    <ul class="space-y-0.5 max-h-80 overflow-y-auto">
       <li :if={@non_owners == []} class="text-sm opacity-70 italic">
-        No other members in the space to transfer ownership to.
+        No admins in the space to transfer ownership to.
       </li>
 
       <li :for={membership <- @non_owners}>
@@ -33,7 +33,7 @@ defmodule WikWeb.SpaceLive.NewOwnerSelector do
             "w-full",
             "opacity-80 hover:opacity-100 transition-all cursor-pointer",
             "flex items-center justify-between gap-1 flex-wrap",
-            "rounded bg-base-300 hover:bg-info/20 px-3 py-2"
+            "rounded bg-base-300 hover:bg-error/50 px-3 py-2"
           ]}
           phx-click={@event_transfer_ownership}
           phx-value-target_membership_id={membership.id}

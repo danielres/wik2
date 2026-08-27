8ab0ba0ee70cd9f427a203d642fa9cb5512f9d98
Daniel Reszka <dannn.r@gmail.com>
2026-08-27 16:15:44 +0200

    PageTreeEditor: feat: add page renaming

--- lib/wik_web/live/page_live/page_rename.ex
+++ lib/wik_web/live/page_live/page_rename.ex
@@ -1,60 +1,46 @@
 defmodule WikWeb.PageLive.PageRename do
   @moduledoc false
 
-  import Phoenix.Component, only: [assign: 2, to_form: 1]
+  import Phoenix.Component, only: [assign: 2]
 
-  alias AshPhoenix.Form
-  alias Wik.Wiki.PageTree
-  alias Wik.Wiki.PageTree.Node
   alias Utils.Log
+  alias Wik.Wiki.PageTree
+  alias WikWeb.Components.Page.RenameFormState
 
-  def assign_defaults(socket), do: assign(socket, page_rename_form: nil)
+  def assign_defaults(socket), do: assign(socket, page_rename: RenameFormState.init())
 
   def open(socket) do
     node = socket.assigns.node
     scope = socket.assigns.current_scope
 
-    form =
-      Node
-      |> Form.for_create(:create,
-        params: %{"slug" => node.slug, "title" => node.title},
-        scope: scope
-      )
-      |> to_form()
-
-    assign(socket, page_rename_form: form)
+    assign(socket, page_rename: RenameFormState.open(node, scope))
   end
 
-  def cancel(socket), do: assign(socket, page_rename_form: nil)
+  def cancel(socket), do: assign(socket, page_rename: RenameFormState.init())
 
   def validate(socket, params) do
-    form = Form.validate(socket.assigns.page_rename_form, params)
-    assign(socket, page_rename_form: form)
+    page_rename = RenameFormState.validate(socket.assigns.page_rename, params)
+    assign(socket, page_rename: page_rename)
   end
 
-  def submit(socket, %{"slug" => slug, "title" => title} = params) do
-    node = socket.assigns.node
+  def submit(socket, params) do
+    page_rename = socket.assigns.page_rename
+    node_id = page_rename.node_id
     page_tree = socket.assigns.page_tree
     scope = socket.assigns.current_scope
 
-    case PageTree.rename_node(page_tree, node.id, slug, title, scope: scope) do
-      {:ok, page_tree} ->
-        path = PageTree.get_node_path(page_tree.nodes, node.id)
+    case RenameFormState.submit(page_rename, page_tree, params, scope) do
+      {:ok, page_rename, page_tree} ->
+        path = PageTree.get_node_path(page_tree.nodes, node_id)
         target = "/#{scope.tenant.slug}/wiki/#{path}"
 
         socket
-        |> assign(page_rename_form: nil)
+        |> assign(page_rename: page_rename)
         |> Phoenix.LiveView.push_navigate(to: target, replace: true)
 
-      {:error, error} ->
+      {:error, page_rename, error} ->
         Log.scoped_error(scope, error, "page_tree rename_node failed")
-
-        form =
-          socket.assigns.page_rename_form
-          |> Form.validate(params)
-          |> Form.add_error(error)
-
-        assign(socket, page_rename_form: form)
+        assign(socket, page_rename: page_rename)
     end
   end
 end
